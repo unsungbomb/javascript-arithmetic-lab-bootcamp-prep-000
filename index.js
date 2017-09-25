1f92cd7c70009d189eda8344192fbc5b46b81926
Sonny McPherson <unsungbomb@gmail.com>
2017-09-25 18:27:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 
-var n = 5
-
 function add(a, b) {
-  return a + b
+  return a + b;
 }
 
 function subtract(a, b) {
-  return a - b
+  return a - b;
 }
 
 function multiply(a, b) {
-  return a * b
+  return a * b;
 }
 
 function divide(a, b) {
-  return a / b
+  return a / b;
 }
 
 function inc(n) {
-  return n++
-  return n
+  n++;
+  return n;
 }
 
 function dec(n) {
-  return n--
-  return n
+  n--;
+  return n;
 }
